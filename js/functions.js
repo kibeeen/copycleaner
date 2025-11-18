df0525bc4090ce98cce378218c27689fd964de88
Kevin <kevincchavez@gmail.com>
2025-11-18 10:48:03 +0800

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -298,7 +298,7 @@ const removables = [
     [/Casino Bonus bonus/gi, 'Casino Bonus'],
     [/Bonus bonus/gi, 'Bonus'],
 
-    [/\[s\]/gi, '(s)'],
+    [/s\[s\]/gi, '(s)'],
     [/s(s)/gi, '(s)'],
 
 
